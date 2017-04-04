1cff956e53a5582c3e5f16399c4a83ac9b4d22e4
Tautvydas <tautvydascoding@gmail.com>
2017-04-04 15:27:10 +0300

    aa

--- uzduotys/uzduotis7/2/js/form-validator.js
+++ uzduotys/uzduotis7/2/js/form-validator.js
@@ -6,21 +6,26 @@
     lastName:  document.querySelector('input[name="lastname"]'),
       email:  document.querySelector('input[name="email"]')
   };
-
-  // uzpildo forma
-  function fillForm (name, surename, email) {
-    formData.firstName.value = name;
-    formData.lastName.value = surename;
-    formData.email.value = email;
+  function validateForm (event) {
+    event.preventDefault(); // uzdraudzia HTML veiksla (submit From)
+    printFromData();
   }
+  function printFromData () {
+    console.log("Duomenys . Vardas: ", formData.firstName.value,
+    " pavarde: ", formData.lastName.value,
+     " email: " , formData.email.value);
+  }
+  form.addEventListener("submit", validateForm);
+
+
+
+  // uzpildo forma - testavimo tikslais
+  // function fillForm (name, surename, email) {
+  //   formData.firstName.value = name;
+  //   formData.lastName.value = surename;
+  //   formData.email.value = email;
+  // }
   // uzpildome forma testiniais duomenimis
   // fillForm("tautvydas", "Kaleda", "a@a.lt");
 
-  
-
-
-
-  console.log("Duomenys . Vardas: ", formData.firstName.value,
-  " pavarde: ", formData.lastName.value,
-   " email: " , formData.email.value);
 })();
