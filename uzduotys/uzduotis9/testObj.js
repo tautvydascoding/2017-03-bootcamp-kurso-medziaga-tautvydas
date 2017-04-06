1ca3327a8536d017fa6c320ac8e274d1424c4480
Tautvydas <tautvydascoding@gmail.com>
2017-04-06 16:27:14 +0300

    as

--- uzduotys/uzduotis9/testObj.js
+++ uzduotys/uzduotis9/testObj.js
@@ -7,7 +7,7 @@ var veikejas = {
   },
   istroskes: false,
   paklydes: false
-}
+};
 var miskas = {
   lokacija: "Gervenupis",
   dydis: 50,
@@ -18,8 +18,9 @@ var miskas = {
    arLyja: false,
    paklaidinti: function () {
      veikejas.paklydes = true;
-   }
-}
+   },
+   temperatura: 5.5
+};
 
 function spausdintiIKonsole ( info) {
   console.log(info);
@@ -41,12 +42,53 @@ function spausdintiIKonsoleSuKomentaru (komnetaras, info) {
 
 
 
+// var nuoVienoIkiSimto = Math.floor(Math.random() * 100) ;
+
+// var atsitiktinisSkaicius = Math.random();
+// if(  atsitiktinisSkaicius > 0.7 ){
+//     spausdintiIKonsoleSuKomentaru("daudaiu nei 0.7", atsitiktinisSkaicius);
+// } else if( atsitiktinisSkaicius <= 0.3 ) {
+//     spausdintiIKonsoleSuKomentaru("maziau ar ba lygu  0.3", atsitiktinisSkaicius);
+// } else {
+//      spausdintiIKonsoleSuKomentaru("nuo 0.31 iki 0.7 ", atsitiktinisSkaicius);
+// }
+
+// var atsitiktinisSkaicius = Math.random();
+// if(  atsitiktinisSkaicius > 0.5 ){
+//     miskas.temperatura = atsitiktinisSkaicius * 10;
+// } else  {
+//       miskas.temperatura = atsitiktinisSkaicius * -10;
+// }
+// spausdintiIKonsoleSuKomentaru("random: ", atsitiktinisSkaicius);
+// spausdintiIKonsoleSuKomentaru("Misko temperatura ", miskas.temperatura);
 
 
+// for()
+// arba
+// while()
 
+// =====sukurti masyva 100 elementu ilgio naudojant FOR
+// var darbuotojoPagamintosDetales = [];
+// for( var i =0; i < 100; i++ ) {
+//   darbuotojoPagamintosDetales[i] = Math.floor(Math.random() * 100);
+// }
 
+//===== sukurti masyva 100 elementu ilgio naudojant WHILE
+// var darbuotojoPagamintosDetales = [];
+// var t = 0;
+// while (darbuotojoPagamintosDetales.length < 100) {
+//     darbuotojoPagamintosDetales[t] = Math.floor(Math.random() * 100);
+//     t++;
+// }
 
+//===== sukurti masyva 100 elementu ilgio naudojant WHILE (2)
+// var darbuotojoPagamintosDetales = new Array(100);
+// while (darbuotojoPagamintosDetales.length < 100) {
+//     darbuotojoPagamintosDetales.push(  Math.floor(Math.random() * 100));
+// }
+spausdintiIKonsoleSuKomentaru("darbuotoju detale: ", darbuotojoPagamintosDetales);
 
+// darbuotojai suveda neteisingas reiksmes(panaudodami minusa)
 
 
 
@@ -57,3 +99,12 @@ function spausdintiIKonsoleSuKomentaru (komnetaras, info) {
 // veikejas.vardas = "Arturas";
 // spausdintiIKonsoleSuKomentaru("Onute  : ", Onute);
 // spausdintiIKonsoleSuKomentaru("veikejas  : ", veikejas);
+
+// === kitas budas OBJ kopijoms
+
+// function Zmogus(vardas, ugis, amzius) {
+//     this.vard = vardas;
+//     this.aukstis = ugis;
+//     this.amz = amzius;
+// }
+// var myFather = new Zmogus("Tautvydass",   150, 23);
