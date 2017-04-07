52e70cd739ef6e490c0a2f74596ccd3eb8d79808
Tautvydas <tautvydascoding@gmail.com>
2017-04-07 15:24:58 +0300

    uzduotis 9 masyvas OBJ

--- uzduotys/uzduotis10/main.js
+++ uzduotys/uzduotis10/main.js
@@ -8,16 +8,30 @@ $(document).ready(function(){
         $(".w3-display-container.w3-container > img").slideToggle(2000);
     });
 
+
     $('img').click(function() {
-       $('img').css("position","absolute");
-       $('img').css("width","20%");
-       $('img').animate(  {
+      var that = this.cloneNode(true);
+      var parent = this.parentNode;
+
+        //  parent.appendChild(that);
+        parent.insertBefore(that,this);
+
+// console.log( image );
+console.log(this.outerHTML);
+console.log(typeof(this.outerHTML));
+console.log(this.parentNode);
+console.log(typeof(this.parentNode));
+
+       $(that).css("position","absolute");
+       $(that).css("width","20%");
+       $(that).animate(  {
             right: '50px',
             top: '50px',
-            opacity: '0.5',
-            height: '150px',
-            width: '150px'
-          },5000);
+            opacity: '0',
+            height: '40px',
+            width: '40px'
+          }, 2000);
+
     });
 
 
