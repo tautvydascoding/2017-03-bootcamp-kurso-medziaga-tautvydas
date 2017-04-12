a2bad988a71fe6f943377e91dc6cbbd3918520c5
Tautvydas <tautvydascoding@gmail.com>
2017-04-12 11:15:55 +0300

    uzduotis 13 rikevimo algoritmas

--- uzduotys/MAMP/uzduotis13-1/index.php
+++ uzduotys/MAMP/uzduotis13-1/index.php
@@ -14,19 +14,36 @@
           $arrayPrice = [213, 12, 1, 31, 645, 165, 213, 534, 1, 4, 1, 132, 6, 3, 35];
 
           print_r ($arrayPrice);
-
           // Boble rikevimo algoritmas
+          // for ( $i = 0; $i < sizeOf($arrayPrice); $i++) {
+          //     for ( $k = 0; $k < sizeOf($arrayPrice) - 1; $k++) {
+          //           if ( $arrayPrice[$k] > $arrayPrice[$k + 1] ) {
+          //             $previous =  $arrayPrice[$k];
+          //             $arrayPrice[$k] = $arrayPrice[$k + 1]; // 213 -> 12
+          //             $arrayPrice[$k + 1] = $previous;    // 12 -> 213
+          //           }
+          //          // pabandyti ta pati funkcijoje su &
+          //     }
+          // }
+
+
+          function swapArrayElements ( &$array,  $curent, $secend ) {
+              $previous =  $array[$curent];  // 213
+              $array[$curent] = $array[$secend]; // 213 -> 12
+              $array[$secend] = $previous;    // 12 -> 213
+          }
           for ( $i = 0; $i < sizeOf($arrayPrice); $i++) {
               for ( $k = 0; $k < sizeOf($arrayPrice) - 1; $k++) {
-                    if ( $arrayPrice[$k] > $arrayPrice[$k + 1] ) {
-                      $previous =  $arrayPrice[$k];
-                      $arrayPrice[$k] = $arrayPrice[$k + 1]; // 213 -> 12
-                      $arrayPrice[$k + 1] = $previous;    // 12 -> 213
+                    if ( $arrayPrice[$k] < $arrayPrice[$k + 1] ) {
+                        swapArrayElements ( $arrayPrice,  $k, $k + 1);
                     }
-                   // pabandyti ta pati funkcijoje su &
               }
           }
+          echo "<br /><br />";
           print_r ($arrayPrice);
+
+
+
        ?>
 
 
