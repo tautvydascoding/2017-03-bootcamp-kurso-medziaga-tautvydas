4477b9ab9a8e7af29276225970a4da7a407ada7a
Tautvydas <tautvydascoding@gmail.com>
2017-04-12 11:46:52 +0300

    uzduotis 13-2 class and obj basics

--- uzduotys/MAMP/uzduotis13-1/index.php
+++ uzduotys/MAMP/uzduotis13-1/index.php
@@ -27,10 +27,10 @@
           // }
 
 
-          function swapArrayElements ( &$array,  $curent, $secend ) {
+          function swapArrayElements (  &$array,  $curent, $secend ) {
               $previous =  $array[$curent];  // 213
               $array[$curent] = $array[$secend]; // 213 -> 12
-              $array[$secend] = $previous;    // 12 -> 213
+              $array[$secend] = $previous;    // 12 -> 213 
           }
           for ( $i = 0; $i < sizeOf($arrayPrice); $i++) {
               for ( $k = 0; $k < sizeOf($arrayPrice) - 1; $k++) {
