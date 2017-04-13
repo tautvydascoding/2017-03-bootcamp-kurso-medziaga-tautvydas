fda9c3cb21ef6128d46af2c39a96a18ee1bac1b1
Tautvydas <tautvydascoding@gmail.com>
2017-04-13 12:05:40 +0300

    uzduotis 13-4 ingeritance

--- uzduotys/MAMP/uzduotis14-1/index.php
+++ uzduotys/MAMP/uzduotis14-1/index.php
@@ -10,18 +10,45 @@
     <?php
 
       interface Human {
-        public function getName() ;
+        public function getName( ) ;
         public function getPersonalCode() ;
       }
-      // class Man implements Human {
-      //   private $testosterone = 3232;
-      //   public function snore() {
-      //   }
-      // }
-      class Woman {
+      class Man implements Human {
+        private $name = "Test";
+        private $testosterone = 3232;
+        public function snore() {
+        }
+        public function getName() {
+          return $this->name;
+        }
+        // mes kalida nes Overloding neveikia PHP
+        // public function getName($aa) {
+        // }
+        public function setName($text) {
+          $this->name = $text;
+        }
+        public function getPersonalCode() {}
+      }
+      $juozas = new Man;
+      echo $juozas->getName()."<br />";
+      $juozas->setName("Juozas");
+      echo $juozas->getName()."<br />";
+
+
+      abstract class HumanType {
+        public $skinType;
+        private $religion;
+        abstract function getSpeed($asaa) ;
+      }
+      class Woman extends HumanType {
+        public $skinType = "#f25631";
         private $estrogenai = 31654;
-        public function gimdymas() {
+        function getSpeed($text) {
+          // kazkoks kodas
+        }
+      public function gimdymas() {
           $this->sumazintiEstro();
+          echo "sumazino Estrog.";
         }
         private function sumazintiEstro() {
           $this->estrogenai -= 10;
@@ -30,6 +57,14 @@
             $this->estrogenai =  $this->estrogenai + 10;
         }
       }
+      class Test extends Woman {
+        public function gimdymas() {
+          echo "isvaziuoju";
+        }
+      }
+      $test = new Test;
+      $test->gimdymas()."<br />";
+
       $Marija = new Woman;
       $Marija->gimdymas();
       // $Marija->sumazintiEstro(); // privaciu funkciju neiseina issaukti!
@@ -37,12 +72,7 @@
 
 
 
-
-
-        // private $asmensKodas;
-        // public $name;
-        // public $lname;
-        // public $gendar;
+ 
      ?>
 
   </body>
